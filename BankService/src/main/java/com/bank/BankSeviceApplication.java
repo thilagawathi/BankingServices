7d35a4651872f3d46a7f11e63115cc8035c9da4f
Ramya ramar <ramya@developer.com>
2021-07-04 20:01:11 +0530

    add SpringBootServletInitializer add goals in pom remove profiles on pom.xml in run configuration

--- BankService/src/main/java/com/bank/BankSeviceApplication.java
+++ BankService/src/main/java/com/bank/BankSeviceApplication.java
@@ -2,9 +2,10 @@ package com.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankSeviceApplication {
+public class BankSeviceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankSeviceApplication.class, args);
